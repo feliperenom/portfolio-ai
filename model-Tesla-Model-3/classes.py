4be08fca42a25c5dd72afd70d4062756ee465de7
feliperenom <feliperenom70@gmail.com>
2025-05-15 20:28:58 -0300

    fg

--- model-Tesla-Model-3/classes.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-# Modelo para la solicitud
-class ChatRequest(BaseModel):
-    prompt: str
--- model-TinyHome/web_search.py
+++
@@ -1,5 +0,0 @@
-#from langchain_community.tools.tavily_search import TavilySearchResults
-#import os
-
-#def setup_web_search_tool():
-#    return TavilySearchResults(max_results=5, api_key=os.getenv("TAVILY_API_KEY"))
